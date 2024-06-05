ff950861f7c276cf224161e505816edd2893002d
GuilhermeCorsino21 <guilhermecorsino21@gmail.com>
2024-06-05 13:16:23 -0300

    api criada

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,12 @@
-import { FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply } from "fastify";
+import fastify, { FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply } from "fastify";
+import { CreateCustomerController } from "./controllers/CreateCustomerController";
 
 export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions) {
     fastify.get("/teste", async (resquest: FastifyRequest, reply: FastifyReply) => {
         return { ok: 'hello world' }
+    })
+
+    fastify.post("/customer,", async (request: FastifyRequest, reply: FastifyReply) => {
+        return new CreateCustomerController().handle(request, reply)
     })
 }
